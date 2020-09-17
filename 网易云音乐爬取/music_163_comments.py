c8bdc03c4de1332b679445c991524ee0b0292185
huanghe1227 <52347001+huanghe1227@users.noreply.github.com>
2020-09-17 23:58:37 +0800

    Update music_163_comments.py

--- 网易云音乐爬取/music_163_comments.py
+++ 网易云音乐爬取/music_163_comments.py
@@ -15,20 +15,14 @@ def get_crypto(data):
 
     return result
 
-
-
-def get_comment(page, id):
-    if page == 1:
-        cursor = -1
-    else:
-        cursor = int(time.time() * 1000)
+def get_comment(page, id, cursor):
     data = {
         "rid": "R_SO_4_" + f"{id}",
         "threadId": "R_SO_4_" + f"{id}",
         "pageNo": f"{page}",
         "pageSize": "20",
         "cursor": f"{cursor}",
-        "offset": f"{(page - 1) * 20}",
+        "offset": "0",
         "orderType": "1",
         "csrf_token": ""
     }
@@ -56,19 +50,25 @@ def get_comment_api(data):
 
 # 解析普通评论
 def parse_comment_json(data, music_id):
-    comments = data["data"]["comments"]
-    for comment in comments:
-        content = comment["content"]
-        content = re.sub('\s', ' ', content)
-        nickname = comment["user"]["nickname"]
-        userId = comment["user"]["userId"]
-        comment_time = comment["time"]
-        timeStamp = int(comment_time/1000)
-        dateArray = datetime.datetime.fromtimestamp(timeStamp)
-        otherStyleTime = dateArray.strftime("[%Y--%m--%d %H:%M:%S]")
-        print(nickname + '(id: ' + str(userId) + ')' + ':  ' + content + otherStyleTime)
-        with open(f'歌曲ID：{music_id}评论.txt', 'a', encoding='utf-8') as f:
-            f.write(nickname + '(id: ' + str(userId) + ')' + ':  ' + otherStyleTime + content + '\n')
+    try:
+        comments = data["data"]["comments"]
+        cursor = data["data"]["cursor"]
+        for comment in comments:
+            content = comment["content"]
+            content = re.sub('\s', ' ', content)
+            nickname = comment["user"]["nickname"]
+            userId = comment["user"]["userId"]
+            comment_time = comment["time"]
+            timeStamp = int(comment_time/1000)
+            dateArray = datetime.datetime.fromtimestamp(timeStamp)
+            otherStyleTime = dateArray.strftime("[%Y--%m--%d %H:%M:%S]")
+            print(nickname + '(id: ' + str(userId) + ')' + ':  ' + content + otherStyleTime)
+            with open(f'./data/歌曲ID：{music_id}评论.txt', 'a', encoding='utf-8') as f:
+                f.write(nickname + '(id: ' + str(userId) + ')' + ':  ' + content + otherStyleTime + '\n')
+        return cursor
+
+    except:
+        print(data)
 
 
 #  解析热门评论
@@ -84,18 +84,22 @@ def parse_hot_comment(data):
         dateArray = datetime.datetime.fromtimestamp(timeStamp)
         otherStyleTime = dateArray.strftime("[%Y--%m--%d %H:%M:%S]")
         print(nickname + '(id: ' + str(userId) + ')' + ':  ' + content + otherStyleTime)
-        with open('网易云音乐热评.txt', 'a', encoding='utf-8') as f:
+        with open('./data/网易云音乐热评.txt', 'a', encoding='utf-8') as f:
             f.write(nickname + '(id: ' + str(userId) + ')' + ':  ' + content + otherStyleTime + '\n')
 
 
+
+
 if __name__ == '__main__':
-    music_id_list = [526464293, 1345848098, 1330348068, 355992, 523251118, ]
+    music_id_list = [208902,]
     for music_id in music_id_list:
-        for page in range(1, 11):
-            data = get_comment(page, music_id)
+        cursor = int(time.time() * 1000)
+        for page in range(1, 100):
+            data = get_comment(page, music_id, cursor)
             result = get_crypto(data)
             music_comment = get_comment_api(result)
             if page == 1:
                 parse_hot_comment(music_comment)
-            parse_comment_json(music_comment, music_id)
-            time.sleep(2)
+            cursor = parse_comment_json(music_comment, music_id)
+
+            time.sleep(1)
